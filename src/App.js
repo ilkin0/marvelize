2d65027c58f0b797f2e95c7632bb4bf2ddfc120a
Ilkin <mehdiyevilkin01@gmail.com>
2020-07-06 14:38:05 +0400

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Header from './components/ui/Header'
+import crypto from 'crypto'
+import './App.css'
 
-function App() {
+const ts = new Date().getTime
+const hash = crypto
+  .createHash('md5')
+  .update(
+    ts +
+      process.env.REACT_APP_MARVEL_PRIVATE_KEY +
+      process.env.REACT_APP_MARVEL_API_PUBLIC_KEY
+  )
+  .digest('hex')
+
+const charactersUrl =
+  process.env.REACT_APP_MARVEL_API_BASE_URL +
+  'characters?ts=' +
+  ts +
+  '&apikey=' +
+  process.env.REACT_APP_MARVEL_API_PUBLIC_KEY +
+  '&hash=' +
+  hash
+
+const App = () => {
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(charactersUrl)
+
+      console.log(result.data)
+
+      setItems(result.data)
+      setIsLoading(false)
+    }
+    fetchItems()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
